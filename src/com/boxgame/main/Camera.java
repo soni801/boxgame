dc762b5af2faf56cf272a557280b10c1261b164f
soni801 <soni801@noreply.github.com>
2018-10-24 20:33:26 +0200

    Changed Java version to 1.8.0u191

--- src/com/boxgame/main/Camera.java
+++ src/com/boxgame/main/Camera.java
@@ -34,48 +34,48 @@ public class Camera
         switch (game.level)
         {
             case 1 :
-                if (x >= 304) x = 304;
-                if (y >= 533) y = 533;
+                if (x >= 294) x = 294;
+                if (y >= 523) y = 523;
                 break;
             case 2 :
-                if (x >= 688) x = 688;
-                if (y >= 917) y = 917;
+                if (x >= 678) x = 678;
+                if (y >= 907) y = 907;
                 break;
             case 3 :
-                if (x >= 944) x = 944;
-                if (y >= 1173) y = 1173;
+                if (x >= 934) x = 934;
+                if (y >= 1163) y = 1163;
                 break;
             case 4 :
-                if (x >= 1200) x = 1200;
-                if (y >= 1429) y = 1429;
+                if (x >= 1190) x = 1190;
+                if (y >= 1419) y = 1419;
                 break;
             case 5 :
-                if (x >= 1456) x = 1456;
-                if (y >= 1685) y = 1685;
+                if (x >= 1446) x = 1446;
+                if (y >= 1675) y = 1675;
                 break;
             case 6 :
-                if (x >= 1712) x = 1712;
-                if (y >= 1941) y = 1941;
+                if (x >= 1702) x = 1702;
+                if (y >= 1931) y = 1931;
                 break;
             case 7 :
-                if (x >= 1840) x = 1840;
-                if (y >= 2069) y = 2069;
+                if (x >= 1830) x = 1830;
+                if (y >= 2059) y = 2059;
                 break;
             case 8 :
-                if (x >= 1840) x = 1840;
-                if (y >= 2069) y = 2069;
+                if (x >= 1830) x = 1830;
+                if (y >= 2059) y = 2059;
                 break;
             case 9 :
-                if (x >= 1840) x = 1840;
-                if (y >= 2069) y = 2069;
+                if (x >= 1830) x = 1830;
+                if (y >= 2059) y = 2059;
                 break;
             case 10 :
-                if (x >= 1840) x = 1840;
-                if (y >= 2069) y = 2069;
+                if (x >= 1830) x = 1830;
+                if (y >= 2059) y = 2059;
                 break;
             case 11 :
-                if (x >= 1968) x = 1968;
-                if (y >= 2197) y = 2197;
+                if (x >= 1958) x = 1958;
+                if (y >= 2187) y = 2187;
         }
     }
 
